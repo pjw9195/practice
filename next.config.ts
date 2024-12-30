da13e02462b6a141d1778878bee182188fa1a54d
Ryan/박진우 <pjw1126@infinistar.co.kr>
2024-12-30 11:22:10 +0900

    feat: next js setting

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      // Basic redirect
+      {
+        source: "/",
+        destination: "/FAQ",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
